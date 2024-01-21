3ead06640e5e9c4678d217380317e030543ea121
MEHEDI HASAN PATOWARY <hasanmdrifat77@gmail.com>
2024-01-21 22:26:46 +0600

    first commit

---
+++ case.js
@@ -0,0 +1,17 @@
+const userName = 'blackPink';
+const userInput = 'BlackPink';
+
+// if(userName.toLowerCase === userInput.toLowerCase){
+//     console.log('valid user');
+// }
+// else{
+//     console.log('Invalid User');
+// }
+
+
+if(userName.toUpperCase === userInput.toUpperCase){
+    console.log('valid user');
+}
+else{
+    console.log('Invalid User');
+}
